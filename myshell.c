48cec085bf85836a1a90c50f2944e8cf3d0dc5e3
Ian Roberts <ian.roberts.6@us.af.mil>
2025-07-14 16:25:36 -0400

    Added comments to main method.

--- myshell.c
+++ myshell.c
@@ -21,24 +21,25 @@ int main(int argc, const char *argv[]) {
 
 	while (exit_sent == 0) {
 		char user_input[100];
-		fgets(user_input, sizeof(user_input), stdin);
-		if (user_input[0] == '\n') {
+		fgets(user_input, sizeof(user_input), stdin);	//Read user input
+		if (user_input[0] == '\n') {					//Check for empty input
 			continue;
 		}
 		strip_trailing_newline(user_input);
-		int num_tokens = get_num_tokens(user_input);
+		int num_tokens = get_num_tokens(user_input);	//Tokenize input
 		char** input_tokens = parse_user_input(user_input, num_tokens);
 		
-		if (strcmp("exit", input_tokens[0]) == 0) {
+		if (strcmp("exit", input_tokens[0]) == 0) {	//Exit token sent
 			exit_sent = 1;
-		} else {
+		} 
+		else {
 			printf("User input has %d tokens\n", num_tokens);
 			for (int i = 0; i < num_tokens; i++) {
 				printf("%s\n", input_tokens[i]);
 			}
 		}
 		
-		free_string_array(input_tokens, num_tokens);
+		free_string_array(input_tokens, num_tokens);	//Clear string array between uses.
 	}
 	printf("Bye bye now!\n");
 	return 0;
