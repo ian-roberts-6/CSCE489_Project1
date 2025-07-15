decefa124f873b204b3b7828e56bc101f727e80c
Ian Roberts <ian.roberts.6@us.af.mil>
2025-07-15 06:24:24 -0400

    First pass at implementing shell functions. Compiles without errors or warnings, but not tested yet.

--- shellfuncts.c
+++ shellfuncts.c
@@ -4,6 +4,8 @@
  *************************************************************************************/
 
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 #include "shellfuncts.h"
 
 
@@ -13,7 +15,14 @@
  * Prints an error message and returns 1 if the file already exists.
  */
 int create(char* filename) {
-	return 1;
+	//TODO - print an error message if the file already exists. (maybe fopen with read?)
+	FILE *opened_file = fopen(filename, "a");
+	if (opened_file == NULL) {
+		perror("Error creating file.");
+		return 1;
+	}
+	fclose(opened_file);
+	return 0;
 }
 
 /**
@@ -22,7 +31,16 @@ int create(char* filename) {
  * Prints an error message and returns 1 if the file does not exist.
  */
 int update(char* filename, int number, char* text) {
-	return 1;
+	//TODO - check for existence of file.
+	FILE *file = fopen(filename, "a");
+	int length = strlen(text);
+	for (int i = 0; i < number; i++) {
+		fprintf(file, "%s\n", text);
+		fflush(file);
+		sleep(length/5);
+	}
+	fclose(file);
+	return 0;
 }
 
 /**
@@ -31,13 +49,23 @@ int update(char* filename, int number, char* text) {
  * Prints an error message and returns 1 if the file does not exist.
  */
 int list(char* filename) {
-	return 1;
+	int length = strlen(filename);
+	char command[length + 5];
+	FILE *file = fopen(filename, "r");
+	if (file == NULL) {
+		perror("File does not exist");
+		return 1;
+	}
+	snprintf(command, length + 5, "cat %s", filename);
+	execl(command, "cat", filename, NULL);
+	fclose(file);
+	return 0;
 }
 
 /**
  * dir - Prints a list of all files in the current directory to the screen.
  */
 void dir() {
-
+	execl("/bin/ls", "ls", NULL);
 	return;
 }
